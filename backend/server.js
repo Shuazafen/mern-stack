6f04375e13184a9948141e135a4159d81d2b0ad7
Shuazafen <olomoshuaomozafen@gmail.com>
2026-02-25 23:46:36 +0100

    server file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import  express  from "express";
-import  cors  from "cors";
+import express from "express";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
@@ -7,7 +7,7 @@ import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-  
+
 const app = express()
 const port = 4000
 
@@ -16,19 +16,19 @@ app.use(cors())
 
 connectDB();
 
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/order", orderRouter)
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
 })
 
-//?
+export default app;
